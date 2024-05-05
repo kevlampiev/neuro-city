c1274a56515e394c03d14c6d1a0a041b07829d36
Evlampiev Konstantin <kevlampiev@gmail.com>
2024-05-06 01:44:19 +0300

    added background

---
+++ resources/views/layouts/layout-components/main-menu.blade.php
@@ -0,0 +1,116 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+        {{-- <a class="navbar-brand" href="{{ route('admin.main') }}"> --}}
+        <a class="navbar-brand" href="/">
+
+            <img src="{{asset('logo.png')}}" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            {{config('app.name')}}
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#"
+                       id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Дроиды
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Список дроидов</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item disabled" href="#">Производство</a></li>
+                    </ul>
+                </li>
+
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#"
+                       id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Договоры/расчеты
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Договоры</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Состояние расчетов по
+                                компаниям</a></li>
+                        <li><a class="dropdown-item" href="#">Состояние расчетов по
+                                контрагентам</a></li>
+                        <li><a class="dropdown-item" href="#">Получить
+                                кредитно-лизинговый портфель</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Предстоящие платежи</a>
+                        </li>
+                        <li><a class="dropdown-item" href="#">Платежный календарь на 2
+                                недели</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Предстоящие платежи
+                                12 месяцев</a></li>
+                    </ul>
+                </li>
+
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false">
+                        Справочники
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Компании группы</a></li>
+                        <li><a class="dropdown-item" href="#">Центры финансовой
+                                ответственности</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Типы техники</a></li>
+                        <li><a class="dropdown-item" href="#">Производители</a></li>
+                        <li><a class="dropdown-item" href="#">Местонахождения техники</a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Типы договоров</a></li>
+                        <li><a class="dropdown-item" href="#">Контрагенты</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Страховые компании</a>
+                        </li>
+                        <li><a class="dropdown-item" href="#">Тип страховок</a></li>
+                        {{-- @if (Auth::user()->role=='admin') --}}
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Пользователи</a></li>
+                            <li><a class="dropdown-item" href="#">Роли в системе</a></li>
+                        {{-- @endif --}}
+
+                    </ul>
+                </li>
+
+            </ul>
+            <form class="d-flex" method="GET" action="#">
+                <input class="form-control me-2" type="search" placeholder="глобальный поиск ..." aria-label="Search"
+                       name="globalSearch">
+                <button class="btn btn-outline-info" type="submit">Искать</button>
+            </form>
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @include('layouts.layout-components.user-data-group')
+            </ul>
+
+        </div>
+
+    </div>
+</nav>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,16 +1,5 @@
 @extends("layouts/app")
 
 @section('content')
-    <div class="bg-white"> 
-    reate mode 100755 storage/framework/.gitignore
- create mode 100755 storage/framework/cache/.gitignore
- create mode 100755 storage/framework/cache/data/.gitignore
- create mode 100755 storage/framework/sessions/.gitignore
- create mode 100755 storage/framework/testing/.gitignore
- create mode 100755 storage/framework/views/.gitignore
- create mode 100755 storage/logs/.gitignore
- create mode 100755 tests/Feature/ExampleTest.php
- create mode 100755 tests/TestCase.php
- create mode 100755 tests/Unit/ExampleTest.php
-    </div>
+    <div class="bg-white"> ddd</div>
 @endsection
