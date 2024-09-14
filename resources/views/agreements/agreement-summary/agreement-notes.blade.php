c01c52bb1ce56d75ae90f18905f6980234fe3fa9
Evlampiev Konstantin <kevlampiev@gmail.com>
2024-09-14 23:21:19 +0300

    changed view for agreement notes list

--- resources/views/agreements/agreement-summary/agreement-notes.blade.php
+++ resources/views/agreements/agreement-summary/agreement-notes.blade.php
@@ -1,38 +1,6 @@
-{{-- <div class="row m-1">
-    <div class="col-md-12">
-        <a class="btn btn-outline-info" href="{{route('addAgreementNote', ['agreement'=>$agreement])}}">Добавить
-            заметку</a>
-
-        <div class="notes-container">
-
-            @forelse($agreement->notes as $index=>$note)
-                <div class="card mb-3">
-                    <div class="card-header">
-                        <strong> {{$note->user->name}} </strong> {{$note->created_at}}
-                    </div>
-                    <div class="card-body">
-                        <p>{{$note->description}}</p>
-                    </div>
-                    @if(($note->user_id === Auth::user()->id)||(Auth::user()->role=="admin"))
-                        <div class="card-footer text-muted">
-                            <a href="{{route('editAgreementNote', ['agreement'=>$agreement, 'agreementNote'=>$note])}}" class="mr-5">
-                                &#9998;Изменить </a>
-                            <a href="{{route('deleteAgreementNote', ['agreement'=>$agreement, 'agreementNote' => $note])}}"
-                               onclick="return confirm('Действительно удалить заметку?')"> &#10008;Удалить </a>
-                        </div>
-                    @endif
-                </div>
-            @empty
-                <p class="font-italic text-secondary">Нет заметок по договору</p>
-            @endforelse
-
-        </div>
-
-    </div>
-</div> --}}
 <div class="row m-1">
     <div class="col-md-12">
-        <a class="btn btn-outline-info" href="{{route('addAgreementNote', ['agreement'=>$agreement])}}">Добавить
+        <a class="btn btn-outline-info mb-2" href="{{route('addAgreementNote', ['agreement'=>$agreement])}}">Добавить
             заметку</a>
 
         <div class="notes-container row">
