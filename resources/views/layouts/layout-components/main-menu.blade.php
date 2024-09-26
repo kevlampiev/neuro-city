4fa867dea8ec1c2d9b72cf4e49f0aa18b3a4acfd
Evlampiev Konstantin <kevlampiev@gmail.com>
2024-09-26 17:58:53 +0300

    saved value of big search

--- resources/views/layouts/layout-components/main-menu.blade.php
+++ resources/views/layouts/layout-components/main-menu.blade.php
@@ -118,7 +118,7 @@
             <form class="d-flex" method="POST" action="{{route('bigSearch')}}">
                 @csrf
                 <input class="form-control me-2" type="search" placeholder="глобальный поиск ..." aria-label="Search"
-                       name="searchStr">
+                       name="searchStr" value="{{$searchStr??''}}">
                 <button class="btn btn-outline-info" type="submit">Искать</button>
             </form>
             <ul class="navbar-nav ml-auto">
