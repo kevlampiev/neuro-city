cb1753d07b71633a7b4b5f8630e8c97520245ab7
Evlampiev Konstantin <kevlampiev@gmail.com>
2024-09-26 17:04:26 +0300

    antipattern

--- resources/views/search/components/document.blade.php
+++ resources/views/search/components/document.blade.php
@@ -1,7 +1,10 @@
 <div class="card-body">
     <h5 class="card-title">{{$index}} Документ</h5>
     <p class="card-text">{{$item->obj_text}}</p>
-    @foreach($item->agreements as $agreement)
+    @php
+        $document = app\Model\Document::findOrFail($item->id);
+    @endphp
+    @foreach($document->agreements as $agreement)
         <a href="{{route('agreementSummary', ['agreement'=>$agreement])}}" class="card-link">Догвор {{$agreemens->agr_number}} от {{$agreements->date_open}}</a>
     @endforeach    
 </div>
